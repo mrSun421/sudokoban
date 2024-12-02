d6183be3d63579966679f778f8818fb014d201e9
Joshua Sun <joshuapsun@gmail.com>
2024-12-02 11:56:15 -0500

    added tutorial text

--- menu/menu.gd
+++ menu/menu.gd
@@ -1,13 +1,17 @@
 extends Control
 
 
+@export var tutorialLayer: CanvasLayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# print(get_tree())
 	pass # Replace with function body.
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if tutorialLayer.visible and Input.is_anything_pressed():
+		tutorialLayer.visible = false
+		
 	
 func _on_NewGame_pressed():
 	pass
@@ -15,3 +19,5 @@ func _on_continue_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu/level_select.tscn")
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+func _on_how_to_play_pressed() -> void:
+	tutorialLayer.visible = !tutorialLayer.visible
